98876abdcbb7bf9c77e6744c61f379612c843bd7
mogls <154231651+mogls@users.noreply.github.com>
2025-01-24 15:09:45 +0100

    Initialises priced based on depreciation

--- Projekt/Initialize/Init.java
+++ Projekt/Initialize/Init.java
@@ -1,15 +1,34 @@
 package Initialize;
 
+import Exceptions.StockMarketException;
 import Interfaces.StockMarket;
 import Stocks.MicrosoftStock;
 import Stocks.NvidiaStock;
 
 public class Init {
     static public StockMarket initMarket(StockMarket market) {
-        market.updateStock("Microsoft", new MicrosoftStock());
-        market.updateStock("Nvidia", new NvidiaStock());
+        market.addStock("Microsoft", new MicrosoftStock());
+        market.addStock("Nvidia", new NvidiaStock());
+
+        initStockPrices(market);
 
         return market;
+    }
+
+    static private void initStockPrices(StockMarket market) {
+        for (String stockName: market.getStockNames()) {
+            try {
+                int price = market.getPrice(stockName);
+                double depreciation = market.getDepreciation();
+
+                double amount = price * (1-depreciation);
+
+                market.updateStockPrice(stockName, (int) Math.round(amount));
+
+            } catch (StockMarketException e) {
+                System.out.println("WTH?????");
+            }
+        }
     }
 }
 
